ff365ad44e8a775e91f68590de55eb9f040c66ae
Lucile-hemp <60678402+Lucile-hemp@users.noreply.github.com>
2020-02-05 02:44:02 +0100

    Update AdneomPartition.java

--- AdneomPartition.java
+++ AdneomPartition.java
@@ -1,24 +1,17 @@
 /**
  * @author www.linkedin.com/in/ren%C3%A9e-l-dirry-337b26189/
- *   @see Raymond Barre Toua
  * @package adneomSujet1
  * @version 1.0
- *
  */
-
-
 package adneomSujet1;
 import java.util.LinkedList;
 import java.util.Iterator;
-
 
 /**
  * class AdneomPartition
  * Cette classe implemente l'interface IAdneomPartition
  *  
  * Elle implemente la Liste sous la forme d'une LinkedList d'Object
- * 
- *
  */
 
 public class AdneomPartition implements IAdneomPartition
@@ -26,7 +19,7 @@ public class AdneomPartition implements IAdneomPartition
 	/*
 	 * Private variable
 	 */ 
-	private LinkedList<Object> partition;
+	private LinkedList<Object> mainPartition;
 	
 	/**
 	 * Contructeur:
@@ -36,37 +29,35 @@ public class AdneomPartition implements IAdneomPartition
 	 */
 	public AdneomPartition()
 	{
-		partition = new LinkedList<Object>();
+		mainPartition = new LinkedList<Object>();
 	}
 	
 	/**
 	 * fonction partition
 	 * @param liste LinkedList, taille int
-	 * @return partition LinkedList 
-	 * 
+	 * @return partition LinkedList  
 	 * Implementation: Cree des sous listes
 	 *                 Itere la Liste en parametre
 	 *                 Divide int fois la Liste en parametre
-	 *                 Ajoute chaque sous Liste à une Liste
+	 *                 Ajoute chaque sous Liste Ã  une Liste
 	 *                 Retourne la Liste
-	 *
 	 *   
 	 */
 	
 	public LinkedList<Object> partition(LinkedList<Object> liste, int taille)
 {	
 			
-			/* création d'une sousListe 
-			 * pour partitionner les éléments puis
-			 * à ajouter à this.partition:
+			/* crÃ©ation d'une sousListe 
+			 * pour partitionner les Ã©lÃ©ments puis
+			 * Ã  ajouter Ã  this.partition:
 			 */
 			
 			LinkedList<Object> sousListe = new LinkedList<Object>();
-				Iterator<Object> itr = liste.iterator();
+			Iterator<Object> itr = liste.iterator();
 				
 			while(itr.hasNext())
 			{
-				/* partitionnement des données 
+				/* partitionnement des donnÃ©es 
 				 *en sous-listes de la taille:
 				 */
 				sousListe = new LinkedList<Object>();
@@ -74,18 +65,17 @@ public class AdneomPartition implements IAdneomPartition
 				for(int index=0; index<taille; index++)
 				{	
 					if(itr.hasNext())
-						{
 						sousListe.add(itr.next());
-						}
 				}
 				
-				/* Ajout de chaque sous-liste à 
-				 * la Liste principale à retourner:
+				/* Ajout de chaque sous-liste Ã  
+				 * la Liste principale Ã  retourner:
 				 */
-				this.partition.add(sousListe);
+				 
+				this.mainPartition.add(sousListe);
 			}		
-		// retourne la liste partitionnée:
-		return this.partition;
+		// retourne la liste partitionnÃ©e:
+		return this.mainPartition;
 	}  // END OF METHOD
 	
 }  // END OF CLASS
