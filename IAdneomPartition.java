ccd011ee96404624a20b2969db832dfc4720790e
Lucile-hemp <60678402+Lucile-hemp@users.noreply.github.com>
2020-02-05 02:37:22 +0100

    Update IAdneomPartition.java

--- IAdneomPartition.java
+++ IAdneomPartition.java
@@ -1,6 +1,6 @@
 /**
  * @author www.linkedin.com/in/ren%C3%A9e-l-dirry-337b26189/
- *   @see Raymond Barre Toua
+ * 
  * @package adneomSujet1
  * @version 1.0
  *
@@ -11,7 +11,7 @@ import java.util.LinkedList;
 
 /**
  * Interface IAdneomPartition
- * Declare une seule fonction à implémenter:
+ * Declare une seule fonction Ã  implÃ©menter:
  *  partition()
  *
  *  
@@ -24,7 +24,5 @@ public interface IAdneomPartition {
 	 * @param taille int
 	 * @return partition LinkedList
 	 */
-	
-	
 	public LinkedList<Object> partition(LinkedList<Object> liste, int taille);
 }
